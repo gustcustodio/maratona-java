71dc78f830d85db18b9e00a1c156e68dfd5b2330
gustcustodio <gustcustodio@gmail.com>
2025-02-28 06:56:56 -0300

    first commit

---
+++ src/academy/devdojo/maratonajava/introducao/Aula01OlaMundoDevDojo.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula01OlaMundoDevDojo {
+    // isso é um comentário //
+    /*
+     * isso é um comentário
+     * de múltiplas
+     * linhas
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
