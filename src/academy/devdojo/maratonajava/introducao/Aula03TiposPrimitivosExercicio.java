2c5f3917e0a3e3d1bcb0e9d8160eca4db9892a61
gustcustodio <gustcustodio@gmail.com>
2025-03-01 17:16:44 -0300

    feat: aula 03 - exercício

---
+++ src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.introducao;
+
+/*
+ * Crie variáveis para os campos descritos abaixo entre <> e imprima a seguinte mensagem:
+ * Eu <nome>, morando no endereço <endereço>, confirmo que recebi o salário de <salário>,
+ * na data <data>
+ */
+public class Aula03TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        String name = "Gustavo";
+        String adress = "Rua Ruazinha";
+        String date = "01/03/2025";
+        double salary = 4000.0;
+        System.out.println(
+                "Eu " + name + ", morando no endereço " + adress +
+                        ", confirmo que recebi o salário de " + salary +
+                        ", na data " + date
+        );
+    }
+}
