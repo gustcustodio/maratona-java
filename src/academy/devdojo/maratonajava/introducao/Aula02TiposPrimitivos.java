287d83a937ba00ea9948338fb187148ee7ac3bed
gustcustodio <gustcustodio@gmail.com>
2025-03-01 17:16:22 -0300

    chore: continuação da aula 02 - mais sobre tipos primitivos

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -4,6 +4,16 @@ public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         // int, double, float, char, byte, short, long, boolean //
         int age = 10;
+        long numeroGrande = 1000000L;
+        double salarioDouble = 2000.0;
+        float salarioFloat = 2500.0F;
+        byte idadeByte = 127;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'M';
+        String nome = "Gustavo";
         System.out.println("A idade é " + age);
+        System.out.println(caractere);
     }
 }
